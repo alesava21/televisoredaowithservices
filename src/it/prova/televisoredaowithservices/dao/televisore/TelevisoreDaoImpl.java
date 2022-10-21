a33ca2b1c96bfa1b2bdf5e157d3dd5ba144f1823
System Administrator <root@MacBook-Air-di-Alessandro.local>
2022-10-21 17:20:02 +0200

    commit dei primi metodi di base

--- src/it/prova/televisoredaowithservices/dao/televisore/TelevisoreDaoImpl.java
+++ src/it/prova/televisoredaowithservices/dao/televisore/TelevisoreDaoImpl.java
@@ -1,54 +1,156 @@
 package it.prova.televisoredaowithservices.dao.televisore;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import it.prova.televisoredaowithservices.dao.AbstractMySQLDAO;
 import it.prova.televisoredaowithservices.model.Televisore;
 
-public class TelevisoreDaoImpl extends AbstractMySQLDAO implements TelevisoreDao{
+public class TelevisoreDaoImpl extends AbstractMySQLDAO implements TelevisoreDao {
+
+	@Override
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
 
 	@Override
 	public List<Televisore> list() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		List<Televisore> result = new ArrayList<Televisore>();
+
+		try (Statement ps = connection.createStatement(); ResultSet rs = ps.executeQuery("seelct * from televisore")) {
+
+			while (rs.next()) {
+				Televisore temp = new Televisore();
+				temp.setId(rs.getLong("id"));
+				temp.setMarca(rs.getString("marca"));
+				temp.setModello(rs.getString("modello"));
+				temp.setPollici(rs.getInt("pollici"));
+				temp.setDataProduzione(rs.getDate("dataproduzione"));
+				result.add(temp);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public Televisore get(Long idInput) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		if (idInput == null || idInput < 1) {
+			throw new Exception("Valore di input non ammesso.");
+
+		}
+
+		Televisore result = null;
+
+		try (PreparedStatement ps = connection.prepareStatement("select * from televisore where id=?;")) {
+			ps.setLong(1, idInput);
+
+			try (ResultSet rs = ps.executeQuery()) {
+
+				if (rs.next()) {
+					Televisore temp = new Televisore();
+					temp.setId(rs.getLong("id"));
+					temp.setMarca(rs.getString("marca"));
+					temp.setModello(rs.getString("modello"));
+					temp.setPollici(rs.getInt("pollici"));
+					temp.setDataProduzione(rs.getDate("dataproduzione"));
+
+				} else {
+					result = null;
+				}
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public int update(Televisore input) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		if (input == null || input.getId() == null || input.getId() < 1)
+			throw new Exception("Valore di input non ammesso.");
+
+		int result = 0;
+		try (PreparedStatement ps = connection
+				.prepareStatement("update televisore set marca=?, modello=?, pollici=?, dataproduzione=? where id=?")) {
+
+			ps.setString(1, input.getMarca());
+			ps.setString(2, input.getModello());
+			ps.setInt(3, input.getPollici());
+			ps.setDate(4, new java.sql.Date(input.getDataProduzione().getTime()));
+			result = ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public int insert(Televisore input) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		if (input == null)
+			throw new Exception("Valore di input non ammesso.");
+
+		int result = 0;
+		try (PreparedStatement ps = connection.prepareStatement(
+				"insert into televisore (marca, modello, pollici, dataproduzione) values (?,?,?,?);")) {
+			ps.setString(1, input.getMarca());
+			ps.setString(2, input.getModello());
+			ps.setInt(3, input.getPollici());
+			ps.setDate(4, new java.sql.Date(input.getDataProduzione().getTime()));
+			result = ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
+
 	}
 
 	@Override
 	public int delete(Televisore input) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		if (input == null || input.getId() == null || input.getId() < 1)
+			throw new Exception("Valore di input non ammesso.");
+
+		int result = 0;
+
+		try (PreparedStatement ps = connection.prepareStatement("delete from televisore where id=?")) {
+			ps.setLong(1, input.getId());
+			result = ps.executeUpdate();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 
 	@Override
 	public List<Televisore> findByExample(Televisore input) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
-	}
-
-	@Override
-	public void setConnection(Connection connection) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
--- src/it/prova/televisoredaowithservices/service/televisore/TelevisoreServiceImpl.java
+++ src/it/prova/televisoredaowithservices/service/televisore/TelevisoreServiceImpl.java
@@ -1,72 +1,189 @@
 package it.prova.televisoredaowithservices.service.televisore;
 
+import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
+import it.prova.televisoredaowithservices.connection.MyConnection;
+import it.prova.televisoredaowithservices.dao.Constants;
 import it.prova.televisoredaowithservices.dao.televisore.TelevisoreDao;
 import it.prova.televisoredaowithservices.model.Televisore;
 
 public class TelevisoreServiceImpl implements TelevisoreService {
 
+	private TelevisoreDao televisoreDao;
+
+	public void setTelevisoreDao(TelevisoreDao televisoreDao) {
+		this.televisoreDao = televisoreDao;
+	}
+
 	@Override
 	public List<Televisore> listAll() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		List<Televisore> result = new ArrayList<Televisore>();
+		try (Connection connection = MyConnection.getConnection(Constants.DRIVER_NAME, Constants.CONNECTION_URL)) {
+
+			televisoreDao.setConnection(connection);
+
+			result = televisoreDao.list();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
+
 	}
 
 	@Override
 	public Televisore findById(Long idInput) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if (idInput == null || idInput < 1)
+			throw new Exception("Valore di input non ammesso.");
+
+		Televisore result = null;
+		try (Connection connection = MyConnection.getConnection(Constants.DRIVER_NAME, Constants.CONNECTION_URL)) {
+
+			// inietto la connection nel dao
+			televisoreDao.setConnection(connection);
+
+			// eseguo quello che realmente devo fare
+			result = televisoreDao.get(idInput);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public int aggiorna(Televisore input) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		if (input == null || input.getId() == null || input.getId() < 1)
+			throw new Exception("Valore di input non ammesso.");
+
+		int result = 0;
+		try (Connection connection = MyConnection.getConnection(Constants.DRIVER_NAME, Constants.CONNECTION_URL)) {
+
+			// inietto la connection nel dao
+			televisoreDao.setConnection(connection);
+
+			// eseguo quello che realmente devo fare
+			result = televisoreDao.update(input);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public int inserisciNuovo(Televisore input) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		if (input == null)
+			throw new Exception("Valore di input non ammesso.");
+
+		int result = 0;
+		try (Connection connection = MyConnection.getConnection(Constants.DRIVER_NAME, Constants.CONNECTION_URL)) {
+
+			// inietto la connection nel dao
+			televisoreDao.setConnection(connection);
+
+			// eseguo quello che realmente devo fare
+			result = televisoreDao.insert(input);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public int rimuovi(Televisore input) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		if (input == null || input.getId() == null || input.getId() < 1)
+			throw new Exception("Valore di input non ammesso.");
+
+		int result = 0;
+		try (Connection connection = MyConnection.getConnection(Constants.DRIVER_NAME, Constants.CONNECTION_URL)) {
+
+			// inietto la connection nel dao
+			televisoreDao.setConnection(connection);
+
+			// eseguo quello che realmente devo fare
+			result = televisoreDao.delete(input);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public List<Televisore> findByExample(Televisore input) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		List<Televisore> result = new ArrayList<>();
+		try (Connection connection = MyConnection.getConnection(Constants.DRIVER_NAME, Constants.CONNECTION_URL)) {
+
+			// inietto la connection nel dao
+			televisoreDao.setConnection(connection);
+
+			// eseguo quello che realmente devo fare
+			result = televisoreDao.findByExample(input);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public List<Televisore> televisoriIntervalloDate(Date primaData, Date secondaData) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		List<Televisore> listaTelevisori = new ArrayList<Televisore>();
+
+		try (Connection connection = MyConnection.getConnection(Constants.DRIVER_NAME, Constants.CONNECTION_URL)) {
+
+			televisoreDao.setConnection(connection);
+
+			listaTelevisori = televisoreDao.televisoriIntervalloDate(primaData, secondaData);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return listaTelevisori;	}
 
 	@Override
 	public List<Televisore> findTelevisoreConPiuPollici(int pollici) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		List<Televisore> listaTelevisori = new ArrayList<Televisore>();
+
+		try (Connection connection = MyConnection.getConnection(Constants.DRIVER_NAME, Constants.CONNECTION_URL)) {
+
+			televisoreDao.setConnection(connection);
+
+			listaTelevisori = televisoreDao.findTelevisoreConPiuPollici(pollici);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return listaTelevisori;
 	}
 
 	@Override
 	public List<Televisore> findMarcaTelevisoriProdottiNeiUltimiSeiMesi(Date dataSeiMesi) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		List<Televisore> listaTelevisori = new ArrayList<Televisore>();
 
-	@Override
-	public void setTelevisoreDao(TelevisoreDao televisoreDao) {
-		// TODO Auto-generated method stub
-		
-	}
+		try (Connection connection = MyConnection.getConnection(Constants.DRIVER_NAME, Constants.CONNECTION_URL)) {
 
+			televisoreDao.setConnection(connection);
+
+			listaTelevisori = televisoreDao.findMarcaTelevisoriProdottiNeiUltimiSeiMesi(dataSeiMesi);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return listaTelevisori;
+	}
 
 }
